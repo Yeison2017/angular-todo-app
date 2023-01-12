c8d0b52eb2d8a5ad250c2ea86bea79f7d90dde15
yeison2017 <yha.big@gmail.com>
2023-01-11 22:59:19 -0500

    Se crea un servicio para manejar los Todos

---
+++ src/app/shared/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  todos: Todo[] = [];
+
+  constructor() {}
+
+  getAllTodos() {
+    return this.todos;
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  uodateTodo(index: number, updatedTodo: Todo) {
+    this.todos[index] = updatedTodo;
+  }
+
+  deleteTodo(index: number) {
+    this.todos.slice(index, 1);
+  }
+}
